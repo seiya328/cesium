a43cbdfd6bb0f74545f7f3949c9f8a941e7e33b0
seiya328 <49935321+seiya328@users.noreply.github.com>
2019-04-24 16:00:40 +0900

    Add files via upload

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -10,6 +10,22 @@ var viewer = new Cesium.Viewer('cesiumContainer');
       }
   });
 
+  var redLine = viewer.entities.add({
+    name:"九頭竜川",
+    description:"ここは九頭竜川です。",　
+    polyline : {
+        positions : Cesium.Cartesian3.fromDegreesArrayHeights([136.137600,36.218795,0,
+          136.146183,36.210070,0,
+          136.147385,36.197465,0,
+          136.155109,36.191923,0,
+          136.157513,36.188183,0,
+          136.154938,36.182364,0,
+          136.146870,36.175574,0]),
+        width : 5,
+        material : Cesium.Color.RED,
+    }
+});
+
   viewer.zoomTo(viewer.entities);　//レイヤにズーム);
 
 

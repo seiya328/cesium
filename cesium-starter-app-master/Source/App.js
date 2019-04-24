3c857c0674a4465f2db3caea695077366cf8bbd3
seiya328 <49935321+seiya328@users.noreply.github.com>
2019-04-24 15:46:39 +0900

    Add files via upload

--- cesium-starter-app-master/Source/App.js
+++ cesium-starter-app-master/Source/App.js
@@ -1,4 +1,16 @@
 var viewer = new Cesium.Viewer('cesiumContainer');
+ 
+  var point = viewer.entities.add({
+    name:"福井市", //レイヤ名
+    description:"ここは福井市です。",　//レイヤの説明
+      position : Cesium.Cartesian3.fromDegrees(136.223554,36.061957,0), //経度,緯度,高さ
+      point : {
+          pixelSize : 10, //ポイントのサイズ
+          color : Cesium.Color.BLUE //ポイントの色
+      }
+  });
+
+  viewer.zoomTo(viewer.entities);　//レイヤにズーム);
 
 
 //kml読み込み
